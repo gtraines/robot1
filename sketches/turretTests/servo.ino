3082b23ebde23571cf19faefb0d13b0c697c38f2
gtraines <gtraines@hotmail.com>
2018-05-31 09:08:57 -0700

    Correct serial print

--- sketches/turretTests/servo.ino
+++ sketches/turretTests/servo.ino
@@ -10,14 +10,16 @@ int IN1=24;
 int IN2=25;
 #define TRAVERSE_SERVO_PIN 5 
 #define TRAVERSE_START_POS 90
-#define TRAVERSE_INCREMENT_DELAY 30
+#define TRAVERSE_INCREMENT_DELAY 15
+String locationUpdate;
 
 void setup() {
+  Serial.begin(115200);
   traverseServo.attach(TRAVERSE_SERVO_PIN);
   traverseServo.write(TRAVERSE_START_POS);
   delay(1000);
   traversePosition = TRAVERSE_START_POS;
-  Serial.write("Moved to initial position");
+  Serial.println(F("Moved to initial position"));
   pinMode(IN1,OUTPUT);
   pinMode(IN2,OUTPUT);
   pinMode(ENA,OUTPUT);
@@ -25,16 +27,19 @@ void setup() {
 }
 
 void loop() {
-  for (traversePosition = traversePosition; traversePosition <= 180; traversePosition += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    traverseServo.write(traversePosition);              // tell servo to go to position in variable 'pos'
-    delay(TRAVERSE_INCREMENT_DELAY);                       // waits 15ms for the servo to reach the position
-    Serial.write("Moved to %d", traversePosition);
+  for (traversePosition = traversePosition; traversePosition < 180; traversePosition += 1) {
+    traverseServo.write(traversePosition);
+    delay(TRAVERSE_INCREMENT_DELAY);
+    locationUpdate = String("Moved to: ") + String(traversePosition);
+    Serial.println(locationUpdate);
+    delay(1);
   }
-  for (traversePosition = traversePosition; traversePosition >= 0; traversePosition -= 1) { // goes from 180 degrees to 0 degrees
-    traverseServo.write(traversePosition);              // tell servo to go to position in variable 'pos'
-    delay(TRAVERSE_INCREMENT_DELAY);                       // waits 15ms for the servo to reach the position
-    Serial.write("Moved to %d", traversePosition);
+  for (traversePosition = traversePosition; traversePosition > 0; traversePosition -= 1) {
+    traverseServo.write(traversePosition);
+    delay(TRAVERSE_INCREMENT_DELAY);
+    locationUpdate = String("Moved to: ") + String(traversePosition);
+    Serial.println(locationUpdate);
+    delay(1);
   }
 }
 
