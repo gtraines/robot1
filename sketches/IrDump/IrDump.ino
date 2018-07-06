c82b3a32b1cc266daea6330f27e4708f4daf8be2
gtraines <gtraines@hotmail.com>
2018-07-06 07:55:04 -0700

    IR sensor progress

--- sketches/IrDump/IrDump.ino
+++ sketches/IrDump/IrDump.ino
@@ -11,22 +11,29 @@
 #include <IRremote.h>
 
 #define HIT_PIN 26
-#define DIR_1_PIN 28
-#define DIR_2_PIN 30
-#define DIR_3_FRONT_PIN 32
-#define DIR_4_PIN 34
+#define DIR_1_REAR_PIN 28
+#define DIR_2_RIGHT_PIN 30
+#define DIR_3_LEFT_PIN 32
+#define DIR_4_FRONT_PIN 34
+#define DIR_5_FLOATING_PIN 36
+
+#define RED_HIT 33EBDD7B
+#define BLUE_HIT A3F34992
+#define PURPLE_HIT1 33EBDD7A
 
 IRrecv _hitSxr(HIT_PIN);
-IRrecv _dir1Sxr(DIR_1_PIN);
-IRrecv _dir2Sxr(DIR_2_PIN);
-IRrecv _dir3Sxr(DIR_3_FRONT_PIN);
-IRrecv _dir4Sxr(DIR_4_PIN);
+IRrecv _dir1Sxr(DIR_1_REAR_PIN);
+IRrecv _dir2Sxr(DIR_2_RIGHT_PIN);
+IRrecv _dir3Sxr(DIR_3_LEFT_PIN);
+IRrecv _dir4Sxr(DIR_4_FRONT_PIN);
+IRrecv _dir5Sxr(DIR_5_FLOATING_PIN);
 
 decode_results _hitResults;
 decode_results _dir1Results;
 decode_results _dir2Results;
 decode_results _dir3Results;
 decode_results _dir4Results;
+decode_results _dir5Results;
 
 void setup()
 {
@@ -35,10 +42,11 @@ void setup()
 
     Serial.begin(115200);
     pinMode(HIT_PIN, INPUT_PULLUP);
-    pinMode(DIR_1_PIN, INPUT_PULLUP);
-    pinMode(DIR_2_PIN, INPUT_PULLUP);
-    pinMode(DIR_3_FRONT_PIN, INPUT_PULLUP);
-    pinMode(DIR_4_PIN, INPUT_PULLUP);
+    pinMode(DIR_1_REAR_PIN, INPUT_PULLUP);
+    pinMode(DIR_2_RIGHT_PIN, INPUT_PULLUP);
+    pinMode(DIR_3_LEFT_PIN, INPUT_PULLUP);
+    pinMode(DIR_4_FRONT_PIN, INPUT_PULLUP);
+    pinMode(DIR_5_FLOATING_PIN, INPUT_PULLUP);
 
     // Start the receivers
     _hitSxr.enableIRIn();
@@ -46,6 +54,7 @@ void setup()
     _dir2Sxr.enableIRIn();
     _dir3Sxr.enableIRIn();
     _dir4Sxr.enableIRIn();
+    _dir5Sxr.enableIRIn();
 }
 
 // Dumps out the decode_results structure.
@@ -55,52 +64,12 @@ void setup()
 //  decode_results *results = (decode_results *)v
 void dump(String sensorName, decode_results *results) {
     Serial.print("Sensor name: ");
-    Serial.println(sensorName);
-  int count = results->rawlen;
-  if (results->decode_type == UNKNOWN) {
-    Serial.print("Unknown encoding: ");
-  } 
-  else if (results->decode_type == NEC) {
-    Serial.print("Decoded NEC: ");
-  } 
-  else if (results->decode_type == SONY) {
-    Serial.print("Decoded SONY: ");
-  } 
-  else if (results->decode_type == RC5) {
-    Serial.print("Decoded RC5: ");
-  } 
-  else if (results->decode_type == RC6) {
-    Serial.print("Decoded RC6: ");
-  }
-  else if (results->decode_type == PANASONIC) {	
-    Serial.print("Decoded PANASONIC - Address: ");
-    Serial.print(results->panasonicAddress,HEX);
+    Serial.print(sensorName);
     Serial.print(" Value: ");
-  }
-  else if (results->decode_type == LG) {
-     Serial.print("Decoded LG: ");
-  }
-  else if (results->decode_type == JVC) {
-     Serial.print("Decoded JVC: ");
-  }
-  Serial.print(results->value, HEX);
-  Serial.print(" (");
-  Serial.print(results->bits, DEC);
-  Serial.println(" bits)");
-  Serial.print("Raw (");
-  Serial.print(count, DEC);
-  Serial.print("): ");
-
-  for (int i = 0; i < count; i++) {
-    if ((i % 2) == 1) {
-      Serial.print(results->rawbuf[i]*USECPERTICK, DEC);
-    } 
-    else {
-      Serial.print(-(int)results->rawbuf[i]*USECPERTICK, DEC);
-    }
-    Serial.print(" ");
-  }
-  Serial.println("");
+    Serial.print(results->value, HEX);
+    Serial.print(" (");
+    Serial.print(results->bits, DEC);
+    Serial.println(")");
 }
 
 
@@ -114,26 +83,34 @@ void loop() {
   if (_dir1Sxr.decode(&_dir1Results)) {
         digitalWrite(31, LOW);
         Serial.println(_dir1Results.value, HEX);
-        dump("Dir 1 Sensor", &_dir1Results);
+        dump("DIR_1_REAR_PIN", &_dir1Results);
         _dir1Sxr.resume(); // Receive the next value
   }
   if (_dir2Sxr.decode(&_dir2Results)) {
         digitalWrite(31, LOW);
         Serial.println(_dir2Results.value, HEX);
-        dump("Dir 2 Sensor", &_dir2Results);
+        dump("DIR_2_RIGHT_PIN", &_dir2Results);
         _dir2Sxr.resume(); // Receive the next value
   }
   if (_dir3Sxr.decode(&_dir3Results)) {
         digitalWrite(31, LOW);
         Serial.println(_dir3Results.value, HEX);
-        dump("Dir 3 FRONT Sensor", &_dir3Results);
+        dump("DIR_3_LEFT_PIN", &_dir3Results);
         _dir3Sxr.resume(); // Receive the next value
   }
   if (_dir4Sxr.decode(&_dir4Results)) {
         digitalWrite(31, LOW);
         Serial.println(_dir4Results.value, HEX);
-        dump("Dir 4 Sensor", &_dir4Results);
+        dump("DIR_4_FRONT_PIN", &_dir4Results);
         _dir4Sxr.resume(); // Receive the next value
   }
+  if (_dir5Sxr.decode(&_dir5Results)) {
+        digitalWrite(31, LOW);
+        Serial.println(_dir5Results.value, HEX);
+        dump("DIR_5_FLOATING_PIN", &_dir5Results);
+        _dir5Sxr.resume(); // Receive the next value
+  }
+  
   digitalWrite(31, HIGH);
+  delay(5);
 }
