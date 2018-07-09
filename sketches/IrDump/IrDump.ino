04a1d443edfdbfa824863b4d576021e001876a8a
gtraines <gtraines@hotmail.com>
2018-07-08 20:52:25 -0700

    IR recv work

--- sketches/IrDump/IrDump.ino
+++ sketches/IrDump/IrDump.ino
@@ -15,25 +15,29 @@
 #define DIR_2_RIGHT_PIN 30
 #define DIR_3_LEFT_PIN 32
 #define DIR_4_FRONT_PIN 34
-#define DIR_5_FLOATING_PIN 36
+#define DIR_5_FLOATING_PIN 15
+#define DIR_6_FLOATING_PIN 17
 
-#define RED_HIT 33EBDD7B
+#define RED_HIT 33EBDD7B 34EBDF0E D104AF4A
 #define BLUE_HIT A3F34992
 #define PURPLE_HIT1 33EBDD7A
+String sxrDirection = "";
+boolean sxrValueAvailable = false;
+// IRrecv _hitSxr(HIT_PIN);
+// IRrecv _dir1Sxr(DIR_1_REAR_PIN);
+// IRrecv _dir2Sxr(DIR_2_RIGHT_PIN);
+// IRrecv _dir3Sxr(DIR_3_LEFT_PIN);
+// IRrecv _dir4Sxr(DIR_4_FRONT_PIN);
+// IRrecv _dir5Sxr(DIR_5_FLOATING_PIN);
+IRrecv _dir6Sxr(DIR_6_FLOATING_PIN);
 
-IRrecv _hitSxr(HIT_PIN);
-IRrecv _dir1Sxr(DIR_1_REAR_PIN);
-IRrecv _dir2Sxr(DIR_2_RIGHT_PIN);
-IRrecv _dir3Sxr(DIR_3_LEFT_PIN);
-IRrecv _dir4Sxr(DIR_4_FRONT_PIN);
-IRrecv _dir5Sxr(DIR_5_FLOATING_PIN);
-
-decode_results _hitResults;
+// decode_results _hitResults;
 decode_results _dir1Results;
-decode_results _dir2Results;
-decode_results _dir3Results;
-decode_results _dir4Results;
+// decode_results _dir2Results;
+// decode_results _dir3Results;
+// decode_results _dir4Results;
 decode_results _dir5Results;
+decode_results _dir6Results;
 
 void setup()
 {
@@ -41,20 +45,23 @@ void setup()
     digitalWrite(31, HIGH);
 
     Serial.begin(115200);
-    pinMode(HIT_PIN, INPUT_PULLUP);
-    pinMode(DIR_1_REAR_PIN, INPUT_PULLUP);
-    pinMode(DIR_2_RIGHT_PIN, INPUT_PULLUP);
-    pinMode(DIR_3_LEFT_PIN, INPUT_PULLUP);
-    pinMode(DIR_4_FRONT_PIN, INPUT_PULLUP);
-    pinMode(DIR_5_FLOATING_PIN, INPUT_PULLUP);
+    // pinMode(HIT_PIN, INPUT_PULLUP);        digitalWrite(31, LOW);
 
+    //pinMode(DIR_1_REAR_PIN, INPUT_PULLUP);
+    // pinMode(DIR_2_RIGHT_PIN, INPUT_PULLUP);
+    // pinMode(DIR_3_LEFT_PIN, INPUT_PULLUP);
+    // pinMode(DIR_4_FRONT_PIN, INPUT_PULLUP);
+    //pinMode(DIR_5_FLOATING_PIN, INPUT_PULLUP);
+    pinMode(DIR_6_FLOATING_PIN, INPUT_PULLUP);
+    
     // Start the receivers
-    _hitSxr.enableIRIn();
-    _dir1Sxr.enableIRIn();
-    _dir2Sxr.enableIRIn();
-    _dir3Sxr.enableIRIn();
-    _dir4Sxr.enableIRIn();
-    _dir5Sxr.enableIRIn();
+    // _hitSxr.enableIRIn();
+    //_dir1Sxr.enableIRIn();
+    // _dir2Sxr.enableIRIn();
+    // _dir3Sxr.enableIRIn();
+    // _dir4Sxr.enableIRIn();
+    //_dir5Sxr.enableIRIn();
+    _dir6Sxr.enableIRIn();
 }
 
 // Dumps out the decode_results structure.
@@ -74,43 +81,52 @@ void dump(String sensorName, decode_results *results) {
 
 
 void loop() {
-  if (_hitSxr.decode(&_hitResults)) {
-        digitalWrite(31, LOW);
-        Serial.println(_hitResults.value, HEX);
-        dump("Hit Sensor", &_hitResults);
-        _hitSxr.resume(); // Receive the next value
-  }
-  if (_dir1Sxr.decode(&_dir1Results)) {
-        digitalWrite(31, LOW);
-        Serial.println(_dir1Results.value, HEX);
-        dump("DIR_1_REAR_PIN", &_dir1Results);
-        _dir1Sxr.resume(); // Receive the next value
-  }
-  if (_dir2Sxr.decode(&_dir2Results)) {
-        digitalWrite(31, LOW);
-        Serial.println(_dir2Results.value, HEX);
-        dump("DIR_2_RIGHT_PIN", &_dir2Results);
-        _dir2Sxr.resume(); // Receive the next value
-  }
-  if (_dir3Sxr.decode(&_dir3Results)) {
-        digitalWrite(31, LOW);
-        Serial.println(_dir3Results.value, HEX);
-        dump("DIR_3_LEFT_PIN", &_dir3Results);
-        _dir3Sxr.resume(); // Receive the next value
-  }
-  if (_dir4Sxr.decode(&_dir4Results)) {
-        digitalWrite(31, LOW);
-        Serial.println(_dir4Results.value, HEX);
-        dump("DIR_4_FRONT_PIN", &_dir4Results);
-        _dir4Sxr.resume(); // Receive the next value
-  }
-  if (_dir5Sxr.decode(&_dir5Results)) {
-        digitalWrite(31, LOW);
-        Serial.println(_dir5Results.value, HEX);
-        dump("DIR_5_FLOATING_PIN", &_dir5Results);
-        _dir5Sxr.resume(); // Receive the next value
-  }
-  
-  digitalWrite(31, HIGH);
-  delay(5);
+//   if (_hitSxr.decode(&_hitResults)) {
+//         digitalWrite(31, LOW);
+//         Serial.println(_hitResults.value, HEX);
+//         dump("Hit Sensor", &_hitResults);
+//         _hitSxr.resume(); // Receive the next value
+//   }
+
+//   if (_dir1Sxr.decode(&_dir1Results)) {
+//         sxrDirection = "DIR1";
+//         sxrValueAvailable = true;
+//   }
+//   if (_dir2Sxr.decode(&_dir2Results)) {
+//         digitalWrite(31, LOW);
+//         Serial.println(_dir2Results.value, HEX);
+//         dump("DIR_2_RIGHT_PIN", &_dir2Results);
+//         _dir2Sxr.resume(); // Receive the next value
+//   }
+//   if (_dir3Sxr.decode(&_dir3Results)) {
+//         digitalWrite(31, LOW);
+//         Serial.println(_dir3Results.value, HEX);
+//         dump("DIR_3_LEFT_PIN", &_dir3Results);
+//         _dir3Sxr.resume(); // Receive the next value
+//   }
+//   if (_dir4Sxr.decode(&_dir4Results)) {
+//         digitalWrite(31, LOW);
+//         Serial.println(_dir4Results.value, HEX);
+//         dump("DIR_4_FRONT_PIN", &_dir4Results);
+//         _dir4Sxr.resume(); // Receive the next value
+//   }
+    // if (_dir5Sxr.decode(&_dir1Results)) {
+    //     sxrDirection = "DIR5";
+    //     sxrValueAvailable = true;
+    // }
+    if (_dir6Sxr.decode(&_dir6Results)) {
+        sxrDirection = "DIR6";
+        sxrValueAvailable = true;
+    }
+    if (sxrValueAvailable == true) {
+        //Serial.println(_dir1Results.value, HEX);    
+        dump(sxrDirection, &_dir6Results);
+        sxrValueAvailable = false;
+        //_dir1Sxr.resume(); // Receive the next value
+        //_dir5Sxr.resume(); // Receive the next value
+        _dir6Sxr.resume(); // Receive the next value
+    }
+
+    digitalWrite(31, HIGH);
+    delay(5);
 }
--- sketches/rawRecv/rawRecv.ino
+++ sketches/rawRecv/rawRecv.ino
@@ -7,14 +7,55 @@
 // Recommend only use IRLibRecvPCI or IRLibRecvLoop for best results
 #include <Arduino.h>
 #include <IRLibRecvPCI.h> 
-#define IR_SENSOR_HIT 26
+#define IR_SENSOR_HIT 2
 
 IRrecvPCI myReceiver(IR_SENSOR_HIT);//pin number for the receiver
+
+// BLUE!!!
+// #define RAW_DATA_LEN 18
+// uint16_t rawData[RAW_DATA_LEN]={
+// 	5998, 574, 526, 470, 1550, 470, 486, 562, 
+// 	1498, 470, 442, 582, 526, 470, 526, 498, 
+// 	1526, 1000};
+// #define RAW_DATA_LEN 18
+// uint16_t rawData[RAW_DATA_LEN]={
+// 	6062, 510, 494, 530, 1522, 470, 406, 618, 
+// 	1522, 474, 450, 570, 526, 470, 466, 558, 
+// 	1522, 1000};
+
+// RED RED RED !!!
+// uint16_t rawData[RAW_DATA_LEN]={
+// 	6038, 502, 522, 478, 1498, 514, 522, 482, 
+// 	1538, 474, 518, 506, 430, 566, 1486, 530, 
+// 	486, 1000};
+// Do a cut-and-paste of the following lines into the 
+// designated location in rawSend.ino
+
+// #define RAW_DATA_LEN 18
+// uint16_t rawData[RAW_DATA_LEN]={
+// 	6098, 466, 534, 490, 1526, 466, 450, 574, 
+// 	1522, 494, 514, 486, 526, 470, 1546, 470, 
+// 	530, 1000};
+
+// // PURPLE PURPLE PURPLE!
+// uint16_t rawData[RAW_DATA_LEN]={
+// 	5982, 562, 518, 486, 1530, 482, 518, 490, 
+// 	1530, 482, 482, 538, 486, 538, 1426, 566, 
+// 	1510, 1000};
+// Do a cut-and-paste of the following lines into the 
+// designated location in rawSend.ino
+
+// #define RAW_DATA_LEN 18
+// uint16_t rawData[RAW_DATA_LEN]={
+// 	5974, 566, 514, 490, 1502, 514, 510, 510, 
+// 	1506, 490, 478, 538, 490, 510, 1450, 566, 
+// 	1482, 1000};
+
 
 void setup() {
   Serial.begin(115200);
   delay(2000); while (!Serial); //delay for Leonardo
-  pinMode(IR_SENSOR_HIT, INPUT_PULLUP);
+  //pinMode(IR_SENSOR_HIT, INPUT_PULLUP);
   pinMode(31, OUTPUT);
   digitalWrite(31, HIGH);
   myReceiver.enableIRIn(); // Start the receiver
