e74e9e76293e07ea1768f6e85231645fae053e00
gtraines <gtraines@hotmail.com>
2018-07-29 06:10:11 -0700

    AVR FreeRTOS files

---
+++ scripts/set_arduino_sketchbook_path.sh
@@ -0,0 +1,1 @@
+arduino --pref sketchbook.path=/home/graham/Source/robot1/sketches/ --save-prefs
--- sketches/turretFreeRtos/turretFreeRtos.ino
+++ sketches/turretFreeRtos/turretFreeRtos.ino
@@ -1,23 +1,49 @@
-#include <Arduino_FreeRTOS.h>
 #include <Arduino.h>
+#include <HardwareSerial.h>
+#include <FreeRTOS_AVR.h>
+#include <semphr.h>  // add the FreeRTOS functions for Semaphores (or Flags).
 
-// define two tasks for Blink & AnalogRead
-void TaskBlink( void *pvParameters );
+// Declare a mutex Semaphore Handle which we will use to manage the Serial Port.
+// It will be used to ensure only only one Task is accessing this resource at any time.
+SemaphoreHandle_t xSerialSemaphore;
+const uint16_t intervalTicks = 10;
+// define two Tasks for DigitalRead & AnalogRead
+void TaskDigitalRead( void *pvParameters );
 void TaskAnalogRead( void *pvParameters );
+
+typedef struct
+{
+  int currentPos;
+  int targetPos;
+  int moving;
+  int speed;
+} elevation_state_t;
+
+typedef struct
+{
+  int currentPos;
+  int targetPos;
+  int moving;
+  int speed;
+} traverse_state_t;
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  
+
   Serial.begin(115200);
-  
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+
+  if ( xSerialSemaphore == NULL )  // Check to confirm that the Serial Semaphore has not already been created.
+  {
+    xSerialSemaphore = xSemaphoreCreateMutex();  // Create a mutex semaphore we will use to manage the Serial Port
+    if ( ( xSerialSemaphore ) != NULL )
+      xSemaphoreGive( ( xSerialSemaphore ) );  // Make the Serial Port available for use, by "Giving" the Semaphore.
   }
 
-  // Now set up two tasks to run independently.
+  // Now set up two Tasks to run independently.
   xTaskCreate(
-    TaskBlink
-    ,  (const portCHAR *)"Blink"   // A name just for humans
+    TaskDigitalRead
+    ,  (const portCHAR *)"DigitalRead"  // A name just for humans
     ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
     ,  NULL
     ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
@@ -31,7 +57,7 @@ void setup() {
     ,  1  // Priority
     ,  NULL );
 
-  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
+  // Now the Task scheduler, which takes over control of scheduling individual Tasks, is automatically started.
 }
 
 void loop()
@@ -43,32 +69,64 @@ void loop()
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
 
-void TaskBlink(void *pvParameters)  // This is a task.
+void TaskDigitalRead( void *pvParameters __attribute__((unused)) )  // This is a Task.
 {
-  (void) pvParameters;
+  /*
+    DigitalReadSerial
+    Reads a digital input on pin 2, prints the result to the serial monitor
 
-  // initialize digital LED_BUILTIN on pin 13 as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+    This example code is in the public domain.
+  */
+
+  // digital pin 2 has a pushbutton attached to it. Give it a name:
+  uint8_t pushButton = 2;
+
+  // make the pushbutton's pin an input:
+  pinMode(pushButton, INPUT);
 
   for (;;) // A Task shall never return or exit.
   {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
+    // read the input pin:
+    int buttonState = digitalRead(pushButton);
+
+    // See if we can obtain or "Take" the Serial Semaphore.
+    // If the semaphore is not available, wait 5 ticks of the Scheduler to see if it becomes free.
+    if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
+    {
+      // We were able to obtain or "Take" the semaphore and can now access the shared resource.
+      // We want to have the Serial Port for us alone, as it takes some time to print,
+      // so we don't want it getting stolen during the middle of a conversion.
+      // print out the state of the button:
+      Serial.println(buttonState);
+
+      xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others.
+    }
+
+    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
   }
 }
 
-void TaskAnalogRead(void *pvParameters)  // This is a task.
+void TaskAnalogRead( void *pvParameters __attribute__((unused)) )  // This is a Task.
 {
-  (void) pvParameters;
 
   for (;;)
   {
     // read the input on analog pin 0:
     int sensorValue = analogRead(A0);
-    // print out the value you read:
-    Serial.println(sensorValue);
+
+    // See if we can obtain or "Take" the Serial Semaphore.
+    // If the semaphore is not available, wait 5 ticks of the Scheduler to see if it becomes free.
+    if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
+    {
+      // We were able to obtain or "Take" the semaphore and can now access the shared resource.
+      // We want to have the Serial Port for us alone, as it takes some time to print,
+      // so we don't want it getting stolen during the middle of a conversion.
+      // print out the value you read:
+      Serial.println(sensorValue);
+
+      xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others.
+    }
+
     vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
   }
 }
