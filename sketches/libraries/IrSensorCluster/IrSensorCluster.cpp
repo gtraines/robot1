fd48c159987ad165a103941e907df6713fa9bbde
gtraines <gtraines@hotmail.com>
2018-08-06 22:55:45 -0700

    Implementing IrSensorCluster

--- sketches/libraries/IrSensorCluster/IrSensorCluster.cpp
+++ sketches/libraries/IrSensorCluster/IrSensorCluster.cpp
@@ -4,11 +4,39 @@
 
 IrSensorCluster::IrSensorCluster() {
     this->_currentSensorIndex = 0;
-    
-    this->_sensorArray[0] = NULL;
+
+    this->_sensorArray[0] = this->_createSensorEntry(IR_SXR_HIT, "HIT");
+    this->_sensorArray[1] = this->_createSensorEntry(IR_SXR_DIRECTION_REAR, "REAR");
+    this->_sensorArray[2] = this->_createSensorEntry(IR_SXR_DIRECTION_RIGHT, "RIGHT");
+    this->_sensorArray[3] = this->_createSensorEntry(IR_SXR_DIRECTION_LEFT, "LEFT");
+    this->_sensorArray[4] = this->_createSensorEntry(IR_SXR_DIRECTION_FRONT, "FRONT");
+}
+
+bool IrSensorCluster::checkNextSensor() {
+    if (this->_currentSensorIndex == 4) {
+        this->_currentSensorIndex = 0;
+    } else {
+        this->_currentSensorIndex++;
+    }
+
+
 }
 
 IrSensorCluster::~IrSensorCluster() {
+}
+
+directional_sensor_t* IrSensorCluster::_createSensorEntry(int pinNumber, String sensorName) {
+    
+    pinMode(pinNumber, INPUT_PULLUP);
+    
+    directional_sensor_t* createdSensor = new directional_sensor_t();
+    
+    createdSensor->PinNumber = pinNumber;
+    createdSensor->Name = sensorName;
+    createdSensor->HitSensor = new IrReceiver(pinNumber);
+    createdSensor->HitSensor->enableIRIn();
+
+    return createdSensor;
 }
 
 
--- sketches/libraries/IrSensorCluster/IrSensorCluster.h
+++ sketches/libraries/IrSensorCluster/IrSensorCluster.h
@@ -6,14 +6,15 @@
 #include <IrReceiver.h>
 
 struct directional_sensor_t {
-    int PinNumber;
-    String Name;
-    IrReceiver* HitSensor;
+    int pinNumber;
+    String sensorName;
+    IrReceiver* irSensor;
+    decode_results sensorResults;
 };
 
 struct sensor_reading_t {
-    String SensorName;
-    long ReadingValue;
+    String sensorName;
+    long readingValue;
 };
 
 class IrSensorCluster {
@@ -24,9 +25,9 @@ public:
     sensor_reading_t* getSensorReading();
 private:
     /* data members */
+    directional_sensor_t* _createSensorEntry(int pinNumber, String sensorName);
     int _currentSensorIndex;
     directional_sensor_t* _sensorArray[5];
-
 };
 
 #endif // IR_SENSOR_CLUSTER__H
