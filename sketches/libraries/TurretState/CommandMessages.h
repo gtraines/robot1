dfcf41f62852820f7ea1e315ab8d6576336ea7b7
gtraines <gtraines@hotmail.com>
2018-11-04 03:05:12 -0700

    adding ack/nack data structures

---
+++ sketches/libraries/TurretState/CommandMessages.h
@@ -0,0 +1,49 @@
+#ifndef TURRET_STATE__COMMAND_MESSSAGES__H
+#define TURRET_STATE__COMMAND_MESSAGES__H
+
+#include <Arduino.h>
+
+enum class CommandRxStatus {
+    ACK,
+    NACK
+};
+
+enum class CommandStatus {
+    IN_PROGRESS,
+    UNABLE_TO_COMPLY,
+    ON_HOLD,
+    CANCELED
+};
+
+
+
+class AckMessage {
+public:
+    AckMessage(long cmdId) {
+        this->commandId = cmdId;
+        this->commandRxStatus = CommandRxStatus::ACK;
+        this->commandStatus = CommandStatus::IN_PROGRESS;
+        this->description = "OK";
+    }
+    AckMessage(long cmdId, CommandRxStatus rxStatus, CommandStatus cmdStatus) {
+        this->commandId = cmdId;
+        this->commandRxStatus = rxStatus;
+        this->commandStatus = cmdStatus;
+        this->description = "OK";
+    }
+    AckMessage(long cmdId, CommandRxStatus rxStatus, CommandStatus cmdStatus, String desc) {
+        this->commandId = cmdId;
+        this->commandRxStatus = rxStatus;
+        this->commandStatus = cmdStatus;
+        this->description = desc;
+    }
+    ~AckMessage() {
+        delete description;
+    }
+    long commandId;
+    CommandRxStatus commandRxStatus;
+    CommandStatus commandStatus;
+    String description;
+};
+
+#endif //TURRET_STATE__COMMAND_MESSAGES__H
