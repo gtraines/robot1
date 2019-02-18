fb2a514470766150dd1652252e488c799e7c5aeb
gtraines <gtraines@hotmail.com>
2019-02-17 19:35:33 -0700

    Miss Katy

--- scripts/install_arduino.sh
+++ scripts/install_arduino.sh
@@ -13,6 +13,7 @@ wget https://github.com/arduino/Arduino/releases/download/1.8.5/Arduino-1.8.5.ta
 tar --xz -xf Arduino-1.8.5.tar.xz 
 cd Arduino-1.8.5/build/
 cp ~/Source/robot1/scripts/build_arduino_arm.sh .
+cp ~/Source/robot1/scripts/reference-1.6.6-3.zip shared/
 cd ~/Source/Arduino-1.8.5/build/
 chmod +x ~/Source/Arduino-1.8.5/build/build_arduino_arm.sh
 sudo bash ~/Source/Arduino-1.8.5/build/build_arduino_arm.sh
