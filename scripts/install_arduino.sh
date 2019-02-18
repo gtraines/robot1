c840dbf4301ee7528ebfbe674b65191e11283f93
gtraines <gtraines@hotmail.com>
2019-02-17 22:03:18 -0700

    Degens

--- scripts/install_arduino.sh
+++ scripts/install_arduino.sh
@@ -7,7 +7,8 @@ echo "/*========================*/"
 
 cd ~/Source
 wget https://github.com/arduino/Arduino/releases/download/1.8.5/Arduino-1.8.5.tar.xz
-tar --xz -xf Arduino-1.8.5.tar.xz 
+tar --xz -xf Arduino-1.8.5.tar.xz
+
 cd Arduino-1.8.5/build/
 cp ~/Source/robot1/scripts/build_arduino_arm.sh .
 wget https://downloads.arduino.cc/libastylej-2.05.1-3.zip
@@ -18,6 +19,10 @@ cd shared/
 wget https://downloads.arduino.cc/reference-1.6.6-3.zip
 wget https://downloads.arduino.cc/Galileo_help_files-1.6.2.zip
 wget https://downloads.arduino.cc/Edison_help_files-1.6.2.zip
+
+cd ~/Source/Arduino-1.8.5/build/linux/
+wget https://downloads.arduino.cc/tools/avr-gcc-4.9.2-atmel3.5.4-arduino2-armhf-pc-linux-gnu.tar.bz2
+
 cd ~/Source/Arduino-1.8.5/build/
 chmod +x ~/Source/Arduino-1.8.5/build/build_arduino_arm.sh
 bash ~/Source/Arduino-1.8.5/build/build_arduino_arm.sh
